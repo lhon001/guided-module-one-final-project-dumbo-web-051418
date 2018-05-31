ae3c16484cbc769790c913bc8794235de1178805
github username <github email address>
2018-05-30 22:43:42 -0400

    completed #show_all_flights, #show_all_passengers_flights

--- bin/run.rb
+++ bin/run.rb
@@ -1,23 +1,26 @@
 require_relative '../config/environment'
 require_relative '../cli.rb'
 
-puts Flight.all
-cli = CommandLineInterface.new
-cli.run
+# puts Flight.all
+ cli = CommandLineInterface.new
+# cli.run
+#
+# input = gets.chomp.to_i
+#
+# case input
+# when 1
+#   puts "Show all flights"
+#   puts Flight.find(1).name
+#
+# when 2
+#   puts "Book a ticket"
+#
+# when 3
+#   puts "show all my flights"
+#
+# when 4
+#   puts "print my ticket"
+#
+# end
 
-input = gets.chomp.to_i
-
-case input
-when 1
-  puts "Show all flights"
-  puts Flight.find(1).name
-
-when 2
-  puts "Book a ticket"
-
-when 3
-  puts "show all my flights"
-
-when 4
-  puts "print my ticket"
-end
+p cli.show_all_passengers_flights("Hart", "Val")
--- cli.rb
+++ cli.rb
@@ -10,14 +10,36 @@ class CommandLineInterface
     puts "4. Print my ticket"
     puts ""
     puts "Enter a option number (1-4)"
+    puts "To exit, type 'exit'"
   end
-
-
 
   def run
     greet
     puts ""
     menu
   end
+
+  def show_all_flights
+    Flight.all.each do |flight|
+      puts "|---------------------------------------|"
+      puts "| Airline     | #{flight.name}"
+      puts "|---------------------------------------|"
+      puts "| Board Time  | #{flight.boarding_time}"
+      puts "|---------------------------------------|"
+      puts "| Departure   | #{flight.start_location}"
+      puts "|---------------------------------------|"
+      puts "| Destination | #{flight.end_location}"
+      puts "|---------------------------------------|"
+      puts "| Flight Num. | #{flight.flight_number}"
+      puts "|---------------------------------------|"
+      puts ""
+    end
+  end
+
+  def show_all_passengers_flights(last_name, first_name)
+    my_flights = Passenger.find_by(last_name: last_name, first_name: first_name)
+    my_flights.flights
+  end
+
 
 end
