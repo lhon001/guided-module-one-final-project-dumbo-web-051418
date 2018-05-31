bbf8841f34f9fcb044f1f5a2f70b093247d3263b
github username <github email address>
2018-05-30 22:46:46 -0400

    add book a flight functionality

--- bin/run.rb
+++ bin/run.rb
@@ -1,23 +1,35 @@
 require_relative '../config/environment'
 require_relative '../cli.rb'
 
-puts Flight.all
+# puts Flight.all
 cli = CommandLineInterface.new
-cli.run
+# cli.run
+# p cli.find_flight_by_flight_num(1354)
+# p cli.get_passenger_info
+# p cli.create_a_passenger
+# p Passenger.all
+# p  cli.book_a_flight
+p Ticket.all
+# input = gets.chomp.to_i
 
-input = gets.chomp.to_i
-
-case input
-when 1
-  puts "Show all flights"
-  puts Flight.find(1).name
-
-when 2
-  puts "Book a ticket"
-
-when 3
-  puts "show all my flights"
-
-when 4
-  puts "print my ticket"
-end
+# case input
+# when 1
+#   puts "Show all flights"
+#   Flight.find(1).name
+#
+# when 2
+#   puts "Book a ticket"
+#
+# when 3
+#   puts "show all my flights"
+#
+# when 4
+#   puts "print my ticket"
+# else
+#   exit
+# end
+#
+# i =0
+# until  == 10
+#
+# end
--- cli.rb
+++ cli.rb
@@ -12,12 +12,49 @@ class CommandLineInterface
     puts "Enter a option number (1-4)"
   end
 
-
+  def book_a_flight
+    flight_id = find_flight_by_flight_num(1354)
+    passenger = create_a_passenger
+    Ticket.create(ticket_num: random_number_gernerator, passenger_id: passenger.id, flight_id: flight_id)
+  end
 
   def run
     greet
     puts ""
     menu
   end
+
+
+
+####
+# Helper methods
+###
+  # takes a flight num as an argument and return the flight id
+  def find_flight_by_flight_num(flight_num)
+     flight = Flight.find_by(flight_number: flight_num)
+     flight.id
+  end
+
+  def create_a_passenger
+    new_passenger = get_passenger_info
+    Passenger.create(first_name: new_passenger[0], last_name: new_passenger[1], age: new_passenger.last )
+  end
+
+  #Ask the Passenger for their personal info
+  def get_passenger_info
+    info = []
+    puts "Please enter your First Name:"
+    info << gets.chomp
+    puts "Please enter your Last Name:"
+    info << gets.chomp
+    puts "Please enter your  Age:"
+    info << gets.chomp.to_i
+  end
+  def random_number_gernerator
+    1 + rand(10000 + 1)
+  end
+
+
+
 
 end
